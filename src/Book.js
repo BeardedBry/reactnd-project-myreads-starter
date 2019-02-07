272c99b71c5c7a13d62302b936cbc68b07e7cd4c
Brian Burkard <brianburkard@gmail.com>
2019-02-06 21:02:12 -0500

    Added moving book from shelfs and routing

--- src/Book.js
+++ src/Book.js
@@ -4,23 +4,42 @@ import React, { Component } from 'react'
 class Book extends Component {
 
     render(){
+
+        const { book, moveToShelf } = this.props;
+
+        const options = [
+            {value: 'currentlyReading', content: 'Currently Reading'},
+            {value: 'wantToRead', content: 'Want to Read'},
+            {value: 'read', content: 'Read'},
+            {value: 'none', content: 'None'}
+        ]
+
+
         return(
-            <li>
+            <li key={book.id}>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.url }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                 <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>Move to...</option>
+                  <select
+                    value={book.shelf}
+                     onChange={(e) => moveToShelf(e.target.value, book)} //need to add function
+                  >
+                  <option value="move" disabled>Move to...</option>
+                  {options.map(option => (
+                      <option key={option.value} value={option.value}>
+                      {option.content}</option>
+                  ))}
+                    {/* <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
-                    <option value="none">None</option>
+                    <option value="none">None</option> */}
                   </select>
                 </div>
               </div>
-              <div className="book-title">{this.props.title}</div>
-              <div className="book-authors">{this.props.author}</div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.author}</div>
             </div>
           </li>
         )
